7304c818a177c2466c8d53a4b184887c9990780a
Abhishek Anand <134626623+aabhisheek@users.noreply.github.com>
2025-07-28 23:15:05 +0530

    oauthpassportjs

---
+++ oauthpassport/server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
+require('./passport');
+
+const app = express();
+
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false, // set to true if using https
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  }
+}))
+
+const isLoggedIn = (req, res, next) => {
+    if (req.user) {
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const port = process.env.PORT || 5000
+
+app.get("/", (req, res) => {
+    res.json({message: "You are not logged in"})
+})
+
+app.get("/failed", (req, res) => {
+    res.send("Failed")
+})
+app.get("/success",isLoggedIn, (req, res) => {
+    res.send(`Welcome ${req.user.email}`)
+})
+
+app.get('/google',
+    passport.authenticate('google', {
+            scope:
+                ['email', 'profile']
+        }
+    ));
+
+app.get('/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/failed',
+    }),
+    function (req, res) {
+        res.redirect('/success')
+
+    }
+);
+
+app.get("/logout", (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.log('Error destroying session:', err);
+        }
+        res.redirect('/');
+    });
+})
+
+app.listen(port, () => console.log("server running on port" + port))
